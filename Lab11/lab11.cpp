a2886f89c618be8a4440ed6cf1c9a900a4a275cc
Corey McDonough <cmcdonough002@student.butte.edu>
2017-08-23 19:17:12 +0000

    Lab1.1 submission

---
+++ Lab11/lab11.cpp
@@ -0,0 +1,8 @@
+'x' is the number of incorrect guesses
+Set 'x'=0
+The computer randomly generates a number between 1 and 10
+The user inputs a number between 1-10
+If the input matches the randomly generated number, display "10-'x'" points and restart at step 2.
+If incorrect, the computer displays either "higher" or "lower"
+Add one more onto 'x' (x+1)
+Repeat back at step 4
